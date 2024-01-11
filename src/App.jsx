99fde7b58675f7b9790dfe69d36fdc6244cb5596
Mahabub Alam <mahabubalam3004@gmail.com>
2024-01-11 11:58:54 +0600

    ....

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+      <div className="flex flex-col justify-center items-center text-7xl w-full h-screen">
+        <h1>Assalamu Alaikum</h1>
+        <p className="rotate-90">|:)|</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
